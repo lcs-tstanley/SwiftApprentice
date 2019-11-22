44b5fb92c295bbf42dc7f718e4492431e34dac3d
Trent Stanley <tstanley@lcs.on.ca>
2019-11-22 14:33:39 -0500

    Initial Commit

---
+++ SwiftApprentice/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftApprentice
+//
+//  Created by Stanley, Trent on 2019-11-22.
+//  Copyright © 2019 Stanley, Trent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
